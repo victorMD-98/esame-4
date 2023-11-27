0a0b2a6799c192abf7c9c7b7b282c6c3388b53d8
Victor <victor246257@gmail.com>
2023-11-28 00:17:50 +0100

    esame4

---
+++ js.js
@@ -0,0 +1,10 @@
+// let nav= document.querySelector("#navigazione")
+// console.log(nav)
+// nav.addEventListener("scroll",()=>{
+//     setTimeout(()=>{
+//         let botton= document.querySelector("nav a:last-child")
+//         //console.log(botton)
+//         botton.style.background.color= "green";
+//     },5000)
+// })
+
